88b06db940d103c72444e109894f9839a62f425b
louis <louis36rain7@naver.com>
2025-08-06 17:27:20 +0900

    users db add

--- models.py
+++ models.py
@@ -14,7 +14,8 @@ class RuntimeRecord(Base):
 
 class User(Base):
     __tablename__ = "users"
-id   =  Column(Integer, primary_key=True, index=True)
-username = Column(String(50), nullable=False, unique=True, index=True) 
-email = Column(String(255), nullable=False, unique=True, index=True)
-password_hash = Column(String(255), nullable=False)
+    
+    id   =  Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), nullable=False, unique=True, index=True) 
+    email = Column(String(255), nullable=False, unique=True, index=True)
+    password_hash = Column(String(255), nullable=False)
