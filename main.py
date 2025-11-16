264eda982d199f3051b5beee8447d1fe4fd67ac5
Louis <smartgauge@smartgauge.co.kr>
2025-11-16 14:43:36 +0900

    update 1116-3

--- main.py
+++ main.py
@@ -768,7 +768,7 @@ class CommentListOut(BaseModel):
 #---------------------------------------------------------------
 
 @app.post("/community/posts/{username}", response_model=PostOut)
-def create_post(body: PostCreate, db: Session = Depends(get_db), username: str):
+def create_post(username: str, body: PostCreate, db: Session = Depends(get_db)):
     userId = db.query(Community_User.id).filter(Community_User.username == username).scalar()
     if not user:
         raise HTTPException(status_code=404, detail="Invalid username")
