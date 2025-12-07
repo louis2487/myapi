c49323b9fef489f9585650adc5c7c8baab18d743
Louis <smartgauge@smartgauge.co.kr>
2025-12-07 15:39:32 +0900

    update 1207-4

--- main.py
+++ main.py
@@ -1137,16 +1137,16 @@ def create_post_plus(post_type:int, username: str, body: PostCreate, db: Session
             title="새 수다글이 등록되었습니다",
             body=f"{username}님이 새로운 수다글을 작성했습니다: {post.title}",
             post_id=post.id,
-            post_type=3,
             target_user_id=1,
+            post_type=3,
         )
         notify_admin_post(
             db,
             title="새 수다글이 등록되었습니다",
             body=f"{username}님이 새로운 수다글을 작성했습니다: {post.title}",
             post_id=post.id,
-            post_type=3,
             target_user_id=10,
+            post_type=3,
         )
 
     return PostOut(
@@ -2034,7 +2034,7 @@ def get_user_id_by_username(db: Session, username: str):
     return user_id
 
 
-def notify_admin_post(db: Session, title: str, body: str, post_id: int, post_type: int = 3, target_user_id: int):
+def notify_admin_post(db: Session, title: str, body: str, post_id: int, target_user_id: int, post_type: int = 3 ):
     noti = create_notification(
         db,
         user_id=target_user_id,
